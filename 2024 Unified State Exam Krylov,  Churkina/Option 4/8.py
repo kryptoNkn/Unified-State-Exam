eca67d226dbf8b8970c4b95da3182dfa324e8cf5
kryptoNkn <167709735+kryptoNkn@users.noreply.github.com>
2024-05-07 17:29:11 +0300

    8

--- 2024 Unified State Exam Krylov,  Churkina/Option 4/8.py
+++ 2024 Unified State Exam Krylov,  Churkina/Option 4/8.py
@@ -2,10 +2,10 @@ from itertools import product
 
 n = 0
 k = 0
-for i in product("АЕИКЛПР", repeat = 6):
+for i in product("АГЕИЛНРТ", repeat = 5):
     s = ''.join(i)
     n += 1
-    if n % 2 == 0 and s[0] != "К" and s.count('И') >= 2:
+    if n % 2 != 0 and s[0] != 'Т' and 1 <= s.count('Н') <= 2:
         k += 1
 print(k)
 
