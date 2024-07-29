4590b9705def73001f38eded4fb3ef5f6400ec60
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-07-29 14:06:40 +0300

    48397.py

--- Type 14/48397.py
+++ Type 14/48397.py
@@ -1,4 +1,8 @@
+# https://inf-ege.sdamgia.ru/problem?id=48397
+
 for x in range(0, 13):
     a = 1 * 13 ** 0 + 7 * 13 ** 1 + x * 13 ** 2 + 8 * 13 ** 3 + 15 * 17 ** 0 + 13 * 17 ** 1 + x * 17 ** 2 + 3 * 17 ** 3
     if a % 197 == 0:
         print(a // 197)
+
+# Solution: 175
