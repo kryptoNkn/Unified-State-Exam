da3eb9dedbeb8ed6d2c2a8c11b31b8975ed1e585
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-09-09 17:14:04 +0300

    11262

---
+++ Type 5/11262.py
@@ -0,0 +1,16 @@
+# https://inf-ege.sdamgia.ru/problem?id=11262
+
+for n in range(1000, 10000):
+    s = str(n)
+    k1 = int(s[0]) + int(s[1])
+    k2 = int(s[1]) + int(s[2])
+    k3 = int(s[2]) + int(s[3])
+    first = str(k1 + k2 + k3 - max(k1,k2,k3) - min(k1,k2,k3))
+    second = str(max(k1,k2,k3))
+    sum = first + second
+    if sum == '1517':
+        print(n)
+        break
+    
+
+    
