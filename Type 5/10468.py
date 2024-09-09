46bf1a6a65390ffe0245d3d50c51d740aa114900
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-09-09 17:11:29 +0300

    10468

---
+++ Type 5/10468.py
@@ -0,0 +1,11 @@
+# https://inf-ege.sdamgia.ru/problem?id=10468
+
+for n in range(1, 10000):
+    s = bin(n)[2:]
+    s += str(s.count('1') % 2)
+    s += str(s.count('1') % 2)
+    r = int(s, 2)
+    if r > 77:
+        print(n)
+        break
+    
