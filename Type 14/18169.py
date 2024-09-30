f5930c6dae93933f8b81fdbf65bb7a1eee52e54b
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-09-30 20:22:04 +0300

    18169

---
+++ Type 14/18169.py
@@ -0,0 +1,10 @@
+for x in range(100000):
+    n= 3 ** 2000 + 3 **10 - x
+    count = 0
+    while n>0:
+        if n % 3 == 2:
+            count += 1
+        n//=3
+
+    if count == 2000:
+        print(x)
