ef7b5a628f3c1fcc0bbd6c86d8a8fa39c3f2ffd1
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-10-16 19:14:01 +0300

    5

---
+++ 2024 Unified State Exam Krylov,  Churkina/Option 5/5.py
@@ -0,0 +1,16 @@
+for N in range(10000):
+    b = bin(N)[2:]
+    if N % 2 == 0:
+        b+='0'
+    else:
+        b+='1'
+
+    if b.count('1') % 3 == 0:
+        b = b[2:]
+        b = '11' + b
+    else:
+        b = b[2:]
+        b = '10' + b
+    r = int(b,2)
+    if r>=26 and r <28:
+        print(N)
