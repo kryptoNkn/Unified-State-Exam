0d24244e90154ba8d82572e2f1ede28b79d63ba0
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-10-17 08:22:18 +0300

    5

--- 2024 Unified State Exam Krylov,  Churkina/Option 7/5.py
+++ 2024 Unified State Exam Krylov,  Churkina/Option 7/5.py
@@ -1,8 +1,8 @@
-from itertools import product
-k = 0
-for i in product(sorted(set('СОРНЯК')), repeat=6):
-    s = ''.join(i)
-    k+=1
-    if s.count('К') <= 3 and s.count('Я') == 2:
-        print(k)
+for N in range(10000):
+    b = bin(N)[2:]
+    if b.count('1') % 2 == 0:
+        b = b[:len(b)//2] + '1' + b[len(b)//2:]
+    r = int(b,2)
+    if r >= 26:
+        print(N)
         break
