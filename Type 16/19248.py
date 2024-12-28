e2c22f8a0b05b92a4b333cbf7a9ef647376600d0
krypton <167709735+kryptoNkn@users.noreply.github.com>
2024-12-28 23:48:50 +0300

    19248

---
+++ Type 16/19248.py
@@ -0,0 +1,9 @@
+from functools import lru_cache
+@lru_cache(None)
+def f(n):
+    if n < 5: return n
+    if n >= 5: return 2*n*f(n-4)
+
+for i in range(1, 13767):
+    f(i)
+print((f(13766) - 9 * f(13762)) // f(13758))
