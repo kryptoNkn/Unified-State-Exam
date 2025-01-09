66a432fe1f11b2324ea2f794b7387631cf34096f
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-01-09 21:14:00 +0300

    18122

--- Type 14/18122.py
+++ Type 14/18122.py
@@ -1,12 +1,10 @@
-for x in range(1, 5556):
-    n = 5 ** 150 + 5 ** 135 - x
-    count = 0
-    while n > 0:
-        if n % 5 == 4:
-            count += 1
-        n //= 5
+for x in range(1, 5555):
+    s = 5 ** 150 + 5 **135-x
+    k = 0
+    while s != 0:
+        if s % 5 == 4:
+            k+=1
+        s//=5
 
-    if count == 134:
+    if k == 134:
         print(x)
-
-# 3126
