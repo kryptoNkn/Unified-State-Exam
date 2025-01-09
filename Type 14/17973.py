17a222b86c50818c43a22985d52cc3d0de914633
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-01-09 21:14:25 +0300

    17973

--- Type 14/17973.py
+++
@@ -1,6 +0,0 @@
-for x in '0123456789abcdefghijklm':
-    n = int(f"12{x}734", 24) + int(f"8{x}95{x}3", 24) + int(f"24{x}796", 24)
-
-    if n % 23 == 0:
-        quotient = n // 23
-        print(quotient)
