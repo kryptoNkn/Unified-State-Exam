aa301d9147ff36857baf098072bb96a56228b442
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-01-19 14:34:44 +0300

    15320

--- Type 8/15320.py
+++ Type 8/15320.py
@@ -1,10 +1,8 @@
 from itertools import product
-
 k = 0
-for i in product('АПРСУ', repeat = 5):
+for i in product(sorted(set('ПАРУС')),repeat=5):
     s = ''.join(i)
     k+=1
     if s.count('У') <= 1 and 'АА' not in s:
-        print(k,s)
-
-# 131
+        print(k)
+        break
