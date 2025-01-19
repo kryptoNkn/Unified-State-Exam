837a1fa5999bf129e95a25a0aa00c9d68a5128cc
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-01-19 14:41:37 +0300

    15326

--- Type 13/15326.py
+++ Type 13/15326.py
@@ -1,7 +1,7 @@
 from ipaddress import *
-net = ip_network("105.224.200.224/255.255.255.224", False)
-count = 0
+net = ip_network('105.224.200.224/255.255.255.224', False)
+k = 0
 for ip in net:
-    if (bin(int(ip))[2:].zfill(32)).count('1') % 4 == 0:
-        count+=1
-print(count)
+    if (bin(int(ip))[2:].zfill(32)).count('1')%4==0:
+        k+=1
+print(k)
