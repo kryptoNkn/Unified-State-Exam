d3f5a54a4cbd2be6d6a5fb75dd5816437c33ce4e
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-01-19 14:50:28 +0300

    15328

--- Type 14/15328.py
+++ Type 14/15328.py
@@ -1,7 +1,7 @@
-for x in '0123456789abcdefghigklmnopq':
-    n = int(f'123{x}24', 27) + int(f'135{x}78', 27)
-    if n % 26 == 0:
-        print(x,n//26)
+for x in '0123456789ABCDEFGHIJKLMNOPQ':
+    s = int(f"123{x}24",27)+int(f"135{x}78",27)
+    k=0
+    if s %26==0:
+        print(s//26)
+
 # 1213206
-
-
