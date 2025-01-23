0093deb5b632441abc40ae9f34e222b910a0af72
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-01-23 17:55:14 +0300

    19237

--- Type 5/19237.py
+++ Type 5/19237.py
@@ -1,19 +1,16 @@
-def f(N):
-    s=''
-    while N != 0:
-        s=str(N % 3)+s
-        N//=3
+def f(n):
+    s = ''
+    while n != 0:
+        s = str(n%3)+s
+        n//=3
     return s
 
-for N in range(1, 10000):
-    b = f(N)
-    if N % 3==0:
-        b = b + b[-2:]
+for n in range(1, 10000):
+    b = f(n)
+    if n % 3 == 0:
+        b = b+b[-2:]
     else:
-        b_sum = sum(int(x) for x in b)
-        b2 = f(b_sum)
-        b = b + b2
+        b = b + f(sum(map(int,b)))
     r = int(b,3)
-    if r > 220 and r<240:
+    if r > 220 and r < 225:
         print(r)
-# 222
