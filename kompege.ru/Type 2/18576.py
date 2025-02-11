39629e84478e2c52c2696bc434c1608f85bb5770
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-02-11 17:47:40 +0300

    18576

---
+++ kompege.ru/Type 2/18576.py
@@ -0,0 +1,8 @@
+print('x y w z')
+for x in range(2):
+    for y in range(2):
+        for w in range(2):
+            for z in range(2):
+                f = (not(w <= (x == y))) and (z <= x)
+                if f==1:
+                    print(x,y,w,z)
