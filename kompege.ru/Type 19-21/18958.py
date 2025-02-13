67901fceaa61712ecc17b22f96d4e0fed0511190
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-02-13 17:43:17 +0300

    18958

---
+++ kompege.ru/Type 19-21/18958.py
@@ -0,0 +1,8 @@
+def f(s,m):
+    if s >= 666: return m%2==0
+    if m==0: return 0
+    h = [f(s+3,m-1), f(s*3,m-1), f(s+s**2,m-1)]
+    return any(h) if m%2!=0 else all(h)
+print([s for s in range(1,666) if f(s,2)])
+print([s for s in range(1,666) if not f(s,1) and f(s,3)])
+print([s for s in range(1,666) if f(s,4) and not f(s,2)])
