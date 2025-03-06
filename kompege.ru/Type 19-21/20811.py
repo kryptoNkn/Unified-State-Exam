987be3fe27e1ce41f546ac0762e6cb61fb0d84e3
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-03-07 01:19:14 +0300

    20811

---
+++ kompege.ru/Type 19-21/20811.py
@@ -0,0 +1,8 @@
+def f(s,m):
+    if s >= 51: return m%2==0
+    if m==0: return 0
+    h = [f(s+1,m-1), f(s+4,m-1),f(s*2,m-1)]
+    return any(h) if m%2!=0 else all(h)
+print([s for s in range(1,51) if not f(s,1) and f(s,2)])
+print([s for s in range(1,51) if not f(s,1) and f(s,3)])
+print([s for s in range(1,51) if f(s,4) and not f(s,2)])
