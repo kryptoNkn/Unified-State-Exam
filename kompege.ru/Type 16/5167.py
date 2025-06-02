b9299c0b86a437765086220e7ea49ee85fdde414
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-06-02 15:41:25 +0300

    5167

--- kompege.ru/Type 16/5167.py
+++ kompege.ru/Type 16/5167.py
@@ -1,10 +1,9 @@
 from functools import lru_cache
 @lru_cache(None)
 def f(n):
-    if n >= 10000: return n
-    if n < 10000 and n%2==0: return f(n+2)-3
+    if n>=10000: return n
+    if n%2==0: return f(n+2)-3
     return f(n+2)+1
-
-for i in range(10000, 0, -1):
+for i in range(10000,0,-1):
     f(i)
 print(f(94)-f(80))
