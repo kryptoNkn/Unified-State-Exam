9a92d58c3f14d8c677f644385f22c27bd11755b9
krypton <167709735+kryptoNkn@users.noreply.github.com>
2025-06-02 17:49:28 +0300

    17532

--- kompege.ru/Type 19-21/17532.py
+++ kompege.ru/Type 19-21/17532.py
@@ -1,8 +1,9 @@
-def f(a,b,m):
-    if a + b>=65: return m%2==0
-    if m == 0: return 0
-    h = [f(a+1,b,m-1), f(a*3,b,m-1), f(a, b+1, m-1), f(a, b*3,m-1)]
-    return any(h) if (m-1)%2==0 else any(h)
-print([s for s in range(1, 300) if f(6,s,2)])
-print([s for s in range(1, 300) if not f(6,s,1) and f(6,s,3)])
-print([s for s in range(1, 300) if f(6,s,4) and not f(6,s,2)])
+def f(s1,s2,m):
+    if s1+s2 >= 65: return m%2==0
+    if m==0: return 0
+    h = [f(s1+1,s2,m-1),f(s1,s2+1,m-1),f(s1*3,s2,m-1),f(s1,s2*3,m-1)]
+    # return any(h) if m%2!=0 else any(h)
+    return any(h) if m % 2 != 0 else all(h)
+# print(min([s for s in range(1,58) if not f(6,s,1) and f(6,s,2)]))
+print([s for s in range(1,58) if not f(6,s,1) and f(6,s,3)])
+print([s for s in range(1,58) if f(6,s,4) and not f(6,s,2)])
